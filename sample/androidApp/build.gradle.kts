31745c2b0192b0b754088fc07afe522d736a7e81
Rick Clephas <rclephas@gmail.com>
2023-04-04 19:35:53 +0200

    Update Kotlin to 1.8.20

--- sample/androidApp/build.gradle.kts
+++ sample/androidApp/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.4-dev-k1.8.20-RC-88d9f3a8232"
+        kotlinCompilerExtensionVersion = "1.4.4-dev-k1.8.20-f6ae19e64ff"
     }
     packagingOptions {
         resources {
