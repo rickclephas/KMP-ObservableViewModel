69e954fb5d6fe2d3a83f8d72d40e01a3c1c3228a
Rick Clephas <rclephas@gmail.com>
2023-09-13 20:39:51 +0200

    Use default hierarchy template

--- kmm-viewmodel-core/build.gradle.kts
+++ kmm-viewmodel-core/build.gradle.kts
@@ -9,34 +9,34 @@ plugins {
 kotlin {
     explicitApi()
     jvmToolchain(11)
+    applyDefaultHierarchyTemplate()
 
     //region Apple and Android targets
-    val macosX64 = macosX64()
-    val macosArm64 = macosArm64()
-    val iosArm64 = iosArm64()
-    val iosX64 = iosX64()
-    val iosSimulatorArm64 = iosSimulatorArm64()
-    val watchosArm32 = watchosArm32()
-    val watchosArm64 = watchosArm64()
-    val watchosX64 = watchosX64()
-    val watchosSimulatorArm64 = watchosSimulatorArm64()
-    val watchosDeviceArm64 = watchosDeviceArm64()
-    val tvosArm64 = tvosArm64()
-    val tvosX64 = tvosX64()
-    val tvosSimulatorArm64 = tvosSimulatorArm64()
+    listOf(
+        macosX64(), macosArm64(),
+        iosArm64(), iosX64(), iosSimulatorArm64(),
+        watchosArm32(), watchosArm64(), watchosX64(), watchosSimulatorArm64(), watchosDeviceArm64(),
+        tvosArm64(), tvosX64(), tvosSimulatorArm64(),
+    ).forEach {
+        it.compilations.getByName("main") {
+            cinterops.create("KMMViewModelCoreObjC") {
+                includeDirs("$projectDir/../KMMViewModelCoreObjC")
+            }
+        }
+    }
     androidTarget {
         publishLibraryVariants("release")
     }
     //endregion
     //region Other targets
-    val jvm = jvm()
-    val js = js {
+    jvm()
+    js {
         browser()
         nodejs()
     }
-    val linuxArm64 = linuxArm64()
-    val linuxX64 = linuxX64()
-    val mingwX64 = mingwX64()
+    linuxArm64()
+    linuxX64()
+    mingwX64()
     //endregion
 
     sourceSets {
@@ -48,61 +48,52 @@ kotlin {
             }
         }
 
-        val commonMain by getting {
+        commonMain {
             dependencies {
                 api(libs.kotlinx.coroutines.core)
             }
         }
-        val commonTest by getting {
+        commonTest {
             dependencies {
                 implementation(libs.kotlin.test)
             }
         }
 
-        val androidMain by getting {
+        androidMain {
             dependencies {
                 api(libs.androidx.lifecycle.viewmodel.ktx)
             }
         }
 
-        val appleMain by creating {
-            dependsOn(commonMain)
-        }
-        val appleTest by creating {
-            dependsOn(commonTest)
-        }
-        listOf(
-            macosX64, macosArm64,
-            iosArm64, iosX64, iosSimulatorArm64,
-            watchosArm32, watchosArm64, watchosX64, watchosSimulatorArm64, watchosDeviceArm64,
-            tvosArm64, tvosX64, tvosSimulatorArm64
-        ).forEach {
-            getByName("${it.targetName}Main") {
-                dependsOn(appleMain)
-            }
-            getByName("${it.targetName}Test") {
-                dependsOn(appleTest)
-            }
-            it.compilations.getByName("main") {
-                cinterops.create("KMMViewModelCoreObjC") {
-                    includeDirs("$projectDir/../KMMViewModelCoreObjC")
-                }
-            }
-        }
-
         val otherMain by creating {
-            dependsOn(commonMain)
+            dependsOn(commonMain.get())
         }
         val otherTest by creating {
-            dependsOn(commonTest)
+            dependsOn(commonTest.get())
         }
-        listOf(jvm, js, linuxArm64, linuxX64, mingwX64).forEach {
-            getByName("${it.targetName}Main") {
-                dependsOn(otherMain)
-            }
-            getByName("${it.targetName}Test") {
-                dependsOn(otherTest)
-            }
+        jvmMain {
+            dependsOn(otherMain)
+        }
+        jvmTest {
+            dependsOn(otherTest)
+        }
+        jsMain {
+            dependsOn(otherMain)
+        }
+        jsTest {
+            dependsOn(otherTest)
+        }
+        linuxMain {
+            dependsOn(otherMain)
+        }
+        linuxTest {
+            dependsOn(otherTest)
+        }
+        mingwMain {
+            dependsOn(otherMain)
+        }
+        mingwTest {
+            dependsOn(otherTest)
         }
     }
 }
--- sample/shared/build.gradle.kts
+++ sample/shared/build.gradle.kts
@@ -38,24 +38,6 @@ kotlin {
                 implementation(libs.kotlin.test)
             }
         }
-        val iosX64Main by getting
-        val iosArm64Main by getting
-        val iosSimulatorArm64Main by getting
-        val iosMain by creating {
-            dependsOn(commonMain)
-            iosX64Main.dependsOn(this)
-            iosArm64Main.dependsOn(this)
-            iosSimulatorArm64Main.dependsOn(this)
-        }
-        val iosX64Test by getting
-        val iosArm64Test by getting
-        val iosSimulatorArm64Test by getting
-        val iosTest by creating {
-            dependsOn(commonTest)
-            iosX64Test.dependsOn(this)
-            iosArm64Test.dependsOn(this)
-            iosSimulatorArm64Test.dependsOn(this)
-        }
     }
 }
 
