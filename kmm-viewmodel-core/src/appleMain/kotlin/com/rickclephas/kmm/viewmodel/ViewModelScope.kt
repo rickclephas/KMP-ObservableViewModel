837b25b080753b2ab42879ddabe710773066a453
Rick Clephas <rclephas@gmail.com>
2023-09-13 20:14:55 +0200

    Add workaround for KT-22841

--- kmm-viewmodel-core/src/appleMain/kotlin/com/rickclephas/kmm/viewmodel/ViewModelScope.kt
+++ kmm-viewmodel-core/src/appleMain/kotlin/com/rickclephas/kmm/viewmodel/ViewModelScope.kt
@@ -17,6 +17,8 @@ import kotlin.native.ref.WeakReference
  * Holds the [CoroutineScope] of a [KMMViewModel].
  * @see coroutineScope
  */
+@OptIn(ExperimentalMultiplatform::class)
+@AllowDifferentMembersInActual // TODO: Remove workaround for https://youtrack.jetbrains.com/issue/KT-22841
 public actual typealias ViewModelScope = KMMVMViewModelScopeProtocol
 
 /**
