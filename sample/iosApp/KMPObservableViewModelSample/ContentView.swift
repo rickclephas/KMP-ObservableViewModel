1d2d6d2a2e5d538109b41972aa2c78e04eaa383d
Rick Clephas <rclephas@gmail.com>
2025-10-26 09:29:48 +0100

    Display SwiftUI view change counts

--- sample/iosApp/KMPObservableViewModelSample/ContentView.swift
+++ sample/iosApp/KMPObservableViewModelSample/ContentView.swift
@@ -11,6 +11,85 @@ import KMPObservableViewModelSwiftUI
 struct ContentView: View {
     
     @StateViewModel var viewModel = TimeTravelViewModel()
+    @StateObject var counter = Counter()
+
+    var body: some View {
+        Spacer()
+        ChangeCounter(counter.incrementAndGet()) {
+            VStack {
+                ActualTimeView(viewModel: viewModel)
+                Spacer().frame(height: 24)
+                TravelEffectView(viewModel: viewModel)
+                Spacer().frame(height: 24)
+                CurrentTimeView(viewModel: viewModel)
+                Spacer().frame(height: 24)
+                IsFixedTimeView(viewModel: viewModel)
+                Spacer().frame(height: 24)
+                Button("Time travel") {
+                    viewModel.timeTravel()
+                }
+                Spacer().frame(height: 24)
+                Button("Reset") {
+                    viewModel.resetTime()
+                }.foregroundColor(viewModel.isResetDisabled ? Color.red : Color.green)
+            }.frame(minWidth: 0, maxWidth: .infinity)
+        }.padding(.horizontal, 8)
+        Spacer()
+    }
+}
+
+struct ActualTimeView: View {
+    
+    @ObservedViewModel var viewModel: TimeTravelViewModel
+    @StateObject var counter = Counter()
+    
+    var body: some View {
+        ChangeCounter(counter.incrementAndGet()) {
+            VStack {
+                Text("Actual time:")
+                Text(viewModel.actualTime)
+                    .font(.system(size: 20))
+            }
+        }
+    }
+}
+
+struct TravelEffectView: View {
+    
+    @ObservedViewModel var viewModel: TimeTravelViewModel
+    @StateObject var counter = Counter()
+    
+    var body: some View {
+        ChangeCounter(counter.incrementAndGet()) {
+            VStack {
+                Text("Travel effect:")
+                Text(viewModel.travelEffect?.description ?? "nil")
+                    .font(.system(size: 20))
+            }
+        }
+    }
+}
+
+struct CurrentTimeView: View {
+    
+    @ObservedViewModel var viewModel: TimeTravelViewModel
+    @StateObject var counter = Counter()
+    
+    var body: some View {
+        ChangeCounter(counter.incrementAndGet()) {
+            VStack {
+                Text("Current time:")
+                Text(viewModel.currentTime)
+                    .font(.system(size: 20))
+            }
+        }
+    }
+}
+
+struct IsFixedTimeView: View {
+    
+    @ObservedViewModel var viewModel: TimeTravelViewModel
+    @StateObject var counter = Counter()
     
     private var isFixedTimeBinding: Binding<Bool> {
         Binding { viewModel.isFixedTime } set: { isFixedTime in
@@ -21,47 +100,13 @@ struct ContentView: View {
             }
         }
     }
-
+    
     var body: some View {
-        VStack{
-            Spacer()
-            Group {
-                Text("Actual time:")
-                Text(viewModel.actualTime)
-                    .font(.system(size: 20))
+        ChangeCounter(counter.incrementAndGet()) {
+            HStack {
+                Toggle("", isOn: isFixedTimeBinding).labelsHidden()
+                Text("Fixed time")
             }
-            Group {
-                Spacer().frame(height: 24)
-                Text("Travel effect:")
-                Text(viewModel.travelEffect?.description ?? "nil")
-                    .font(.system(size: 20))
-            }
-            Group {
-                Spacer().frame(height: 24)
-                Text("Current time:")
-                Text(viewModel.currentTime)
-                    .font(.system(size: 20))
-            }
-            Group {
-                Spacer().frame(height: 24)
-                HStack {
-                    Toggle("", isOn: isFixedTimeBinding).labelsHidden()
-                    Text("Fixed time")
-                }
-            }
-            Group {
-                Spacer().frame(height: 24)
-                Button("Time travel") {
-                    viewModel.timeTravel()
-                }
-            }
-            Group {
-                Spacer().frame(height: 24)
-                Button("Reset") {
-                    viewModel.resetTime()
-                }.foregroundColor(viewModel.isResetDisabled ? Color.red : Color.green)
-            }
-            Spacer()
         }
     }
 }
