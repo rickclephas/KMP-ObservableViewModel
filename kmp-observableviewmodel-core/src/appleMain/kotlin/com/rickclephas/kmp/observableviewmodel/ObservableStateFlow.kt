7bbae09d5f232b9c59d5dd811af7340bbe04f8d4
Rick Clephas <rclephas@gmail.com>
2025-10-26 16:03:19 +0100

    Implement StateFlow.replayCache directly

--- kmp-observableviewmodel-core/src/appleMain/kotlin/com/rickclephas/kmp/observableviewmodel/ObservableStateFlow.kt
+++ kmp-observableviewmodel-core/src/appleMain/kotlin/com/rickclephas/kmp/observableviewmodel/ObservableStateFlow.kt
@@ -9,7 +9,7 @@ import kotlinx.coroutines.flow.StateFlow
 
 /**
  * A [MutableStateFlow] wrapper that emits state change events through the [NativeViewModelScope]
- * and it accounts for the [NativeViewModelScope.subscriptionCount].
+ * and accounts for the [NativeViewModelScope.subscriptionCount].
  */
 @OptIn(ExperimentalForInheritanceCoroutinesApi::class)
 internal class ObservableMutableStateFlow<T>(
@@ -31,8 +31,10 @@ internal class ObservableMutableStateFlow<T>(
             publisher?.didSet(property)
         }
 
+    // Same implementation as in StateFlowImpl, but we need to go through our own value property.
+    // https://github.com/Kotlin/kotlinx.coroutines/blob/6dfabf763fe9fc91fbb73eb0f2d5b488f53043f1/kotlinx-coroutines-core/common/src/flow/StateFlow.kt#L367
     override val replayCache: List<T>
-        get() = stateFlow.replayCache
+        get() = listOf(value)
 
     /**
      * The combined subscription count from the [NativeViewModelScope] and the actual [StateFlow].
